084540e0ae0b5cc3982d13d91f0192a731e7c8b8
shamspias <shamsuddinpias0@gmail.com>
2023-11-20 17:18:07 +0600

    fix: email valid in paypal

--- app/schemas.py
+++ app/schemas.py
@@ -1,18 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 
 class ReservationBase(BaseModel):
     number_of_people: int
     full_name: str
-    email: str
+    email: EmailStr
     phone_number: str
     address: str
     payment_card_info: str
     stay_duration: int
     room_type: str
+    paypal_email: Optional[EmailStr] = None
+    paypal_password: Optional[str] = None  # Caution: Storing passwords in plain text is not secure
 
 
 class ReservationCreate(ReservationBase):
+    pass
+
+
+class ReservationUpdate(ReservationBase):
     pass
 
 
