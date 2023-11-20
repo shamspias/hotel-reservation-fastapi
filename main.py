808a76c1ebd011094b1a347a1205fe0605a0f0c8
shamspias <shamsuddinpias0@gmail.com>
2023-11-20 16:44:24 +0600

    fix: database config in main

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from app.api import router as api_router
+from app.database import create_database
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    create_database()  # Create database tables
+
 
 app.include_router(api_router)
