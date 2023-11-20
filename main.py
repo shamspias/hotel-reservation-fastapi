6c97a4233395b9768d55ddeb708efa6656afffac
shamspias <shamsuddinpias0@gmail.com>
2023-11-20 17:18:46 +0600

    fix: model intigration and cors

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import router as api_router
 from app.database import create_database
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 @app.on_event("startup")
